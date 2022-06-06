d0dce42594dfa76eb7aaca9bccee142a073d4a91
mwape365 <unratedideas@gmail.com>
2022-06-06 10:39:12 -0700

    Function prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         separator = ""
         for col in row:
-            print("{:s}{:d}".format(separator, col, end="")
-                    separator = " "
-                print()
+            print("{:s}{:d}".format(separator, col), end="")
+            separator = " "
+        print()
