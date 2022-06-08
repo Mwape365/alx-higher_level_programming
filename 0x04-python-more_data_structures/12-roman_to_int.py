3ad236920b085189f7a179366f1c25121497b926
mwape365 <unratedideas@gmail.com>
2022-06-08 10:13:02 -0700

    Function converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-        # Fail checks, none, not a string
+    # Fail checks, none, not a string
     if not roman_string:
         return 0
     if not isinstance(roman_string, str):
